a9fea5d7c6a884ba99b7a5cfac8011f8f087b73e
francgrasso <francgrasso@gmail.com>
2025-01-13 16:09:21 +0100

    added comment form to single post layout - increase footer padding to distinguish it better

--- patterns/footer.php
+++ patterns/footer.php
@@ -6,8 +6,8 @@
  */
 ?>
 <!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"top":{"width":"1px"}}}} -->
+<div class="wp-block-group" style="border-top-width:1px;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
 echo sprintf( esc_html__( 'Proudly Powered by %1$sNewfold Digital%2$s', 'nextgen-theme' ), '<a href="' . esc_url( 'https://newfold.com' ) . '" rel="nofollow">', '</a>' ); ?></p>
 <!-- /wp:paragraph --></div>
